b4a846cd553512d0505eae268213e985c73a87ea
Anas <anas.chaabane98@gmail.com>
2022-11-30 21:33:53 +0100

    implment changes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Posts', type: :request do
     it 'returns http success' do
       get '/users/1/posts/1'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('here is posts for a given user')
     end
   end
 
@@ -12,6 +13,7 @@ RSpec.describe 'Posts', type: :request do
     it 'returns http success' do
       get '/users/1/posts/'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('this where all the posts')
     end
   end
 end

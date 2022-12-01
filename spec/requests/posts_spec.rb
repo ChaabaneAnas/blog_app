2c904f2d923e58d5f26f6b880ce3b64c3d159342
Anas <anas.chaabane98@gmail.com>
2022-12-01 08:10:17 +0100

    add template matcher to request test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
       get '/users/1/posts/1'
+      expect(response).to render_template(:show)
       expect(response).to have_http_status(:success)
       expect(response.body).to include('here is posts for a given user')
     end
@@ -14,6 +15,7 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts/'
       expect(response).to have_http_status(:success)
       expect(response.body).to include('this where all the posts')
+      expect(response).to render_template(:index)
     end
   end
 end

34ff91b8bad5ae938f1fded3a85de940a442c314
Anas <anas.chaabane98@gmail.com>
2022-12-03 13:37:00 +0100

    add hyperlinks for add comment and add like

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :posts,only: [:new, :create,:index, :show] do
       resources :comments, only: [:index, :new, :create, :show]
+      resources :likes, only: [:create]
     end
   end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
